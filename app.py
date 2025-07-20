ea147064e81fa76c50503080cfe9ef39b5a2707c
Avinash Kumar <8404ak@gmail.com>
2025-07-20 23:24:11 +0530

    Update requirements.txt with correct packages

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
 import pickle
 import string
+import nltk # Import nltk at the top
+
+# Add these lines to download NLTK data
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('punkt')
+nltk.download('stopwords') # Also download stopwords, as you're using them
+
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -21,6 +33,7 @@ def transform_text(text):
     y.clear()
 
     for i in text:
+        # Ensure stopwords are loaded before this line
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
 
@@ -32,20 +45,25 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load your pre-trained model and vectorizer
+# Make sure 'vectorizer.pkl' and 'model.pkl' are in the same directory as app.py
+try:
+    tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+    model = pickle.load(open('model.pkl', 'rb'))
+except FileNotFoundError:
+    st.error("Error: 'vectorizer.pkl' or 'model.pkl' not found. Make sure they are in the same directory.")
+    st.stop() # Stop the app if essential files are missing
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
